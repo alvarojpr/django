045c611cf4f9ae70afac96b838f3c25277d826c6
alvarojpr <alvarojosedm9@gmail.com>
2025-02-23 10:49:50 -0300

    Aula 9

--- omg_djangos/views.py
+++ omg_djangos/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Página principal do omg_djangos"""
-
-
-
     return render(request, 'omg_djangos/index.html')
-# Create your views here.
+
+def topics(request):
+    """Motra todos os assuntos"""
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics":topics}
+    return render(request, 'omg_djangos/topics.html', context)
