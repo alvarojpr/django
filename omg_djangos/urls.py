96df4fe5ef3686d50941eefe5ba042f93fc31b96
alvarojpr <alvarojosedm9@gmail.com>
2025-02-23 11:09:24 -0300

    Aula 10

--- omg_djangos/urls.py
+++ omg_djangos/urls.py
@@ -1,7 +1,9 @@
+#aqui são as rotas
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # url
     path('topics', views.topics, name='topics'), # url/topics
+    path('topic/<topic_id>/', views.topic, name='topic'), # url/topics
 ]
--- omg_djangos/views.py
+++ omg_djangos/views.py
@@ -10,3 +10,10 @@ def topics(request):
     topics = Topic.objects.order_by("date_added")
     context = {"topics":topics}
     return render(request, 'omg_djangos/topics.html', context)
+
+def topic(request, topic_id):
+    """Motra um unico assunto pelo id todas as sua entradas"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {"topic": topic, 'entries': entries}
+    return render(request, 'omg_djangos/topic.html', context)
