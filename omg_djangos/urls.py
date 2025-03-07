990eeec50e4c242bd9212ef2f1a12dd388f788b0
alvarojpr <alvarojosedm9@gmail.com>
2025-03-07 16:42:23 -0300

    Aula 15

--- omg_djangos/urls.py
+++ omg_djangos/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), # url
     path('topics', views.topics, name='topics'), # url/topics
-    path('topics/<topic_id>/', views.topic, name='topic'), # url/topics
+    path('topics/<topic_id>/', views.topic, name='topic'), # url/topics/n
     path('new_topic', views.new_topic, name='new_topic'), # url/new_topic
-    path('new_entry/<topic_id>/', views.new_entry, name='new_entry'), # url/new_entry
+    path('new_entry/<topic_id>/', views.new_entry, name='new_entry'), # url/new_entry/n
+    path('edit_entry/<entry_id>', views.edit_entry, name='edit_entry')
 ]
